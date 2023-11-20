ffad17325f7c24556da25cadcfeb358df6c5499e
jm_Han <22000789@handong.ac.kr>
2023-11-20 19:21:40 +0900

    파일추가

---
+++ src/main/java/com/example/jsphelloproject/FriendsDAO.java
@@ -0,0 +1,150 @@
+package com.example.jsphelloproject;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.jsphelloproject.FriendsVO;
+import com.example.jsphelloproject.JDBCUtil;
+
+public class FriendsDAO {
+
+    Connection conn = null;
+    PreparedStatement stmt = null;
+    ResultSet rs = null;
+
+    private final String Friends_INSERT = "insert into friends (name, student_num, major, gender, age, hometown, dormitory, christian, birthday, season) values (?,?,?,?,?,?,?,?,?,?)";
+    private final String Friends_UPDATE = "update friends set name=?, student_num=?, major=?, gender=?, age=?, hometown=?, dormitory=?, christian=?, birthday=?, season=? where seq=?";
+    private final String Friends_DELETE = "delete from friends  where seq=?";
+    private final String Friends_GET = "select * from friends  where seq=?";
+    private final String Friends_LIST = "select * from friends order by seq desc";
+
+    public int insertBoard(FriendsVO vo) {
+        System.out.println("===> JDBC로 insertFriends() 기능 처리");
+        try {
+            conn = JDBCUtil.getConnection();
+            stmt = conn.prepareStatement(Friends_INSERT);
+            stmt.setString(1, vo.getName());
+            stmt.setString(2, vo.getStudent_num());
+            stmt.setString(3, vo.getMajor());
+            stmt.setString(4, vo.getGender());
+            stmt.setInt(5, vo.getAge());
+            stmt.setString(6, vo.getHometown());
+            stmt.setString(7, vo.getDormitory());
+            stmt.setString(8, vo.getChristian());
+            stmt.setDate(9, (Date) vo.getBirthday());
+            stmt.setString(10, vo.getSeason());
+            stmt.executeUpdate();
+            return 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    // 글 삭제
+    public void deleteBoard(FriendsVO vo) {
+        System.out.println("===> JDBC로 deleteFriends() 기능 처리");
+        try {
+            conn = JDBCUtil.getConnection();
+            stmt = conn.prepareStatement(Friends_DELETE);
+            stmt.setInt(1, vo.getSeq());
+            stmt.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public int updateBoard(FriendsVO vo) {
+        System.out.println("===> JDBC로 updateFriends() 기능 처리");
+        try {
+            conn = JDBCUtil.getConnection();
+            stmt = conn.prepareStatement(Friends_UPDATE);
+            stmt.setString(1, vo.getName());
+            stmt.setString(2, vo.getStudent_num());
+            stmt.setString(3, vo.getMajor());
+            stmt.setString(4, vo.getGender());
+            stmt.setInt(5, vo.getAge());
+            stmt.setString(6, vo.getHometown());
+            stmt.setString(7, vo.getDormitory());
+            stmt.setString(8, vo.getChristian());
+            stmt.setDate(9, (Date) vo.getBirthday());
+            stmt.setString(10, vo.getSeason());
+            stmt.executeUpdate();
+            stmt.setInt(4, vo.getSeq());
+
+
+            System.out.println(vo.getName() + "-" + vo.getStudent_num() + "-" + vo.getMajor() + "-"
+                    + vo.getGender() + "-" + vo.getAge() + "-" + vo.getHometown() + "-" + vo.getDormitory() + "-"
+                    + vo.getChristian() + "-" + vo.getBirthday() + "-" + vo.getSeason() + "-" + vo.getSeq());
+            stmt.executeUpdate();
+            return 1;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public FriendsVO getBoard(int seq) {
+        FriendsVO one = new FriendsVO();
+        System.out.println("===> JDBC로 getFriends() 기능 처리");
+        try {
+            conn = JDBCUtil.getConnection();
+            stmt = conn.prepareStatement(Friends_GET);
+            stmt.setInt(1, seq);
+            rs = stmt.executeQuery();
+            if(rs.next()) {
+                one.setSeq(rs.getInt("seq"));
+                one.setName(rs.getString("name"));
+                one.setStudent_num(rs.getString("student_num"));
+                one.setMajor(rs.getString("major"));
+                one.setGender(rs.getString("gender"));
+                one.setAge(rs.getInt("age"));
+                one.setHometown(rs.getString("hometown"));
+                one.setDormitory(rs.getString("dormitory"));
+                one.setChristian(rs.getString("christian"));
+                one.setBirthday(rs.getDate("birthday"));
+                one.setSeason(rs.getString("season"));
+                one.setCnt(rs.getInt("cnt"));
+            }
+            rs.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return one;
+    }
+
+    public List<FriendsVO> getBoardList(){
+        List<FriendsVO> list = new ArrayList<FriendsVO>();
+        System.out.println("===> JDBC로 getFriendsList() 기능 처리");
+        try {
+            conn = JDBCUtil.getConnection();
+            stmt = conn.prepareStatement(Friends_LIST);
+            rs = stmt.executeQuery();
+            while(rs.next()) {
+                FriendsVO one = new FriendsVO();
+                one.setSeq(rs.getInt("seq"));
+                one.setName(rs.getString("name"));
+                one.setStudent_num(rs.getString("student_num"));
+                one.setMajor(rs.getString("major"));
+                one.setGender(rs.getString("gender"));
+                one.setAge(rs.getInt("age"));
+                one.setHometown(rs.getString("hometown"));
+                one.setDormitory(rs.getString("dormitory"));
+                one.setChristian(rs.getString("christian"));
+                one.setBirthday(rs.getDate("birthday"));
+                one.setSeason(rs.getString("season"));
+                one.setRegdate(rs.getDate("regdate"));
+                one.setCnt(rs.getInt("cnt"));
+                list.add(one);
+            }
+            rs.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+}
---
+++ src/main/java/com/example/jsphelloproject/FriendsVO.java
@@ -0,0 +1,123 @@
+package com.example.jsphelloproject;
+
+import java.util.Date;
+
+public class FriendsVO {
+    private int seq;
+    private String name;
+    private String student_num;
+    private String major;
+    private String gender;
+    private int age;
+    private String hometown;
+    private String dormitory;
+    private String christian;
+    private Date birthday;
+    private String season;
+    private Date regdate;
+    private int cnt;
+
+    public int getSeq() {
+        return seq;
+    }
+
+    public void setSeq(int seq) {
+        this.seq = seq;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStudent_num() {
+        return student_num;
+    }
+
+    public void setStudent_num(String student_num) {
+        this.student_num = student_num;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHometown() {
+        return hometown;
+    }
+
+    public void setHometown(String hometown) {
+        this.hometown = hometown;
+    }
+
+    public String getDormitory() {
+        return dormitory;
+    }
+
+    public void setDormitory(String dormitory) {
+        this.dormitory = dormitory;
+    }
+
+    public String getChristian() {
+        return christian;
+    }
+
+    public void setChristian(String christian) {
+        this.christian = christian;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getSeason() {
+        return season;
+    }
+
+    public void setSeason(String season) {
+        this.season = season;
+    }
+
+    public Date getRegdate() {
+        return regdate;
+    }
+
+    public void setRegdate(Date regdate) {
+        this.regdate = regdate;
+    }
+
+    public int getCnt() {
+        return cnt;
+    }
+
+    public void setCnt(int cnt) {
+        this.cnt = cnt;
+    }
+}
